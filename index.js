af81aa25ddd867f06b7dbcf4021309ad5e563e24
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-22 20:22:43 +0700

    xong

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const fs = require('fs');
 
 const Product = require('./Product');
 const upload = require('./uploadConfig');
@@ -29,6 +30,42 @@ app.get('/admin', (req, res) => {
     Product.find({})
     .then(products => res.render('admin', { products }));
 });
+
+app.get('/xoa/:id', (req, res) => {
+    const { id } = req.params;
+    Product.findByIdAndRemove(id)
+    .then((product) => {
+        res.redirect('/admin');
+        if(product.image === 'default.jpg') return;
+        const path = './public/images/background/' + product.image;
+        fs.unlink(path, (err) => {
+            if(err) console.log(err.message);
+        });
+    })
+    .catch(err => res.send(err.message));
+});
+
+app.get('/sua/:id', (req, res) => {
+    const { id } = req.params;
+    Product.findById(id)
+    .then(product => res.render('edit', { product }));
+});
+
+app.post('/update/:id', upload.single('image'), (req, res) => {
+    const { id } = req.params;
+    const { name, video, desc, oldImage } = req.body;
+    const image = req.file ? req.file.filename : oldImage;
+    Product.findByIdAndUpdate(id, {name, video, desc, image })
+    .then(() => {
+        res.redirect('/admin');
+        if(!req.file) return;
+        const path = './public/images/background/' + oldImage;
+        fs.unlink(path, (err) => {
+            if(err) console.log(err.message);
+        });
+    })
+});
+
 
 app.post('/add', upload.single('image'), (req, res) => {
     const { name, video, desc } = req.body;
